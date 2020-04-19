ea749b6b5e8310946743c82ad3fb5acaaf4da3a4
Dasom Won <59657188+wondasom@users.noreply.github.com>
2020-04-19 23:25:54 +0200

    structuring, styling

--- src/Message.js
+++ src/Message.js
@@ -17,85 +17,157 @@ export default class Message extends React.Component {
     message: "",
     toBeReplaced: "",
     replaceWith: "",
-    character: ""
+    character: "",
+    suffix: "",
+    toggle: true
   };
 
-  //when I use 'string' state, it gives error. I don 't know why.. (Sad face)
+  toggleChange = () => {
+    this.setState(state => ({ toggle: !state.toggle }));
+  };
 
   render() {
     return (
       <div>
-        <h1>Message Mixer</h1>
-        <p>
-          Message Mixer is a messaging service that allows you to perform an
-          action on input text and display the output of that behavior to the
-          console. For example, with the current functions defined in Message
-          Mixer, you can:
-        </p>
-        <ul>
-          <li>count the characters in a message</li>
-          <li>capitalize the first character of words</li>
-          <li>reverse a message’s words in place</li>
-          <li>reversing characters in place</li>
-          <li>replace the first occurrence of a string</li>
-          <li>replace all occurrences of a string</li>
-          <li>
-            encode text by swapping certain characters for other characters
-          </li>
-          <li>make a palindrome</li>
-          <li>add a fabricated suffix </li>
-        </ul>
+        <div>
+          <h1>Message Mixer</h1>
+          <div className="instruction">
+            <p>
+              <span>Message Mixer</span> is a messaging service that allows you
+              to perform an action on input text and display the output of that
+              behavior to the console. For example, with the current functions
+              defined in Message Mixer, you can:
+            </p>
+            <ul>
+              <li>
+                <span>1)</span>count the characters in a message
+              </li>
+              <li>
+                <span>2)</span>capitalize the first character of words -
+                titlecase
+              </li>
+              <li>
+                <span>3)</span>reverse a message’s words in place
+              </li>
+              <li>
+                <span>4)</span>reversing characters in place
+              </li>
+              <li>
+                <span>5)</span>replace the first occurrence of a string
+              </li>
+              <li>
+                <span>6)</span>replace all occurrences of a string
+              </li>
+              <li>
+                <span>7)</span>
+                encode text by swapping certain characters for other characters
+              </li>
+              <li>
+                <span>8)</span>make a palindrome
+              </li>
+              <li>
+                <span>9)</span>add a fabricated suffix{" "}
+              </li>
+            </ul>
+          </div>
+        </div>
+
         <div>
           <textarea
+            className="inputMessage"
             rows="10"
             cols="20"
             placeholder="Put your message here"
             onChange={event => this.setState({ message: event.target.value })}
           />
+        </div>
+        <div className="container">
+          <div className="section">
+            <h3>
+              <span>1)</span>Character Count
+            </h3>
+            <p>character:</p>
+            <input
+              className="inputText"
+              onChange={event =>
+                this.setState({ character: event.target.value })
+              }
+            />
 
-          <p>{countCharacter(this.state.message, "t")}</p>
-        </div>
-        <div>
-          <p>
-            {capitalizeFirstCharacterOfWords("What is the color of the sky?")}
-          </p>
-        </div>
-        <div>
-          <p>{reverseWord(this.state.message)}</p>
-        </div>
-        <div>
-          <p>{reverseAllWords(this.state.message)}</p>
-        </div>
-        <div>
-          <input
-            onChange={event =>
-              this.setState({ toBeReplaced: event.target.value })
-            }
-          />
-          <input
-            onChange={event =>
-              this.setState({ replacedWith: event.target.value })
-            }
-          />
-          <p>
-            {replaceFirstOccurence(
-              this.state.message,
-              this.state.toBeReplaced,
-              this.state.replacedWith
-            )}
-          </p>
-        </div>
-        <div>
-          <p>{encode(this.state.message)}</p>
-        </div>
-        <div>
-          <p>{palindrome(this.state.message)}</p>
-        </div>
-        <div>
-          <input
-            onChange={event => this.setState({ character: event.target.value })}
-          />
-          <p>{pigLatin(this.state.message, this.state.character)}</p>
+            <p>
+              <span>{this.state.character}</span> is used{" "}
+              {countCharacter(this.state.message, this.state.character)} times
+              in the message.
+            </p>
+          </div>
+
+          <div className="section">
+            <h3>
+              <span>2)</span>Title-case
+            </h3>
+            <p>
+              {capitalizeFirstCharacterOfWords("What is the color of the sky?")}
+            </p>
+          </div>
+          <div className="section">
+            <h3>
+              <span>3)</span>Reversing Word
+            </h3>
+            <p>{reverseWord(this.state.message)}</p>
+          </div>
+          <div className="section">
+            <h3>
+              <span>4)</span>Reversing All Words
+            </h3>
+            <p>{reverseAllWords(this.state.message)}</p>
+          </div>
+          <div className="section">
+            <h3>
+              <span>5)</span>Replacing Word
+            </h3>
+            <p>word to be replaced:</p>
+            <input
+              className="inputText"
+              onChange={event =>
+                this.setState({ toBeReplaced: event.target.value })
+              }
+            />
+            <p>word replacing with:</p>
+            <input
+              className="inputText"
+              onChange={event =>
+                this.setState({ replacedWith: event.target.value })
+              }
+            />
+            <p>
+              {replaceFirstOccurence(
+                this.state.message,
+                this.state.toBeReplaced,
+                this.state.replacedWith
+              )}
+            </p>
+          </div>
+          <div className="section">
+            <h3>
+              <span>6)</span>Encoding
+            </h3>
+            <p>{encode(this.state.message)}</p>
+          </div>
+          <div className="section">
+            <h3>
+              <span>7)</span>Palindrome
+            </h3>
+            <p>{palindrome(this.state.message)}</p>
+          </div>
+          <div className="section">
+            <h3>
+              <span>8)</span>Adding Suffix
+            </h3>
+            <input
+              onChange={event => this.setState({ suffix: event.target.value })}
+            />
+            <p>{pigLatin(this.state.message, this.state.suffix)}</p>
+          </div>
         </div>
       </div>
     );
